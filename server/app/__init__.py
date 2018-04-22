04dd2364e0f0ba58a41f120727056feaad5fe2f7
Zexin Yan <yzx9610@outlook.com>
2018-04-22 11:18:37 +0800

    impl database

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,24 +1,25 @@
-# coding=utf-8
-"""Initialize `app` module."""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
 
-from flask import Flask
-from config import config
-from .api import api
+engine = None
+Base = None
+session = None
 
 
-def create_app(config_name):
-    """
-    Create the app object.
-
-    :param config_name: type of configuration
-    :return: the app object
-    """
-    app = Flask(__name__, instance_relative_config=True)
-    # load public config
-    app.config.from_object(config[config_name])
-    # load private config at instance/config.py
-    # app.config.from_pyfile('config.py')
-
-    api.init_app(app)
-
-    return app
+def init_db(_user,
+            _password,
+            _db_name,
+            connect_fail_callback):
+    global engine
+    global Base
+    global session
+    try:
+        engine = create_engine('mysql://{}:{}@localhost/{}?charset=utf8'.format(_user,
+                                                                   _password,
+                                                                   _db_name), echo=True)
+        Base = declarative_base()
+        Session = sessionmaker(bind=engine)
+        session = Session()
+    except Exception as err:
+        connect_fail_callback(err)
---
+++ server/app/labels.py
@@ -0,0 +1,79 @@
+from server.app import *
+
+
+class Labels(Base):
+    __tablename__ = 'Labels'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
+    quality = Column(BOOLEAN, default=False)  # 图片质量
+    dr = Column(BOOLEAN)  # 是否有糖尿病视网膜病变
+    stage = Column(Integer)  # 所处阶段[1-7]
+    dme = Column(BOOLEAN)  # 是否有糖尿病性黄斑水肿
+    hr = Column(Integer)  # 高血压视网膜病变 [0-3]
+    age_dme = Column(Integer)  # 年龄相关性黄斑水肿 [0-3]
+    rvo = Column(BOOLEAN)  # 是否有视网膜静脉阻塞
+    crao = Column(BOOLEAN)  # 受有视网膜动脉阻塞
+    od = Column(BOOLEAN)  # 是否有病理性近视
+    glaucoma = Column(BOOLEAN)  # 是否有疑似青光眼
+    others = Column(BOOLEAN)  # 是否有其他疾病
+    comment = Column(TEXT)  # 备注
+
+    def __repr__(self):
+        return '<Labels: id:{}>'.format(self.id)
+
+
+def create_table():
+    Base.metadata.create_all(engine)
+
+
+def add_label(_quality: BOOLEAN,
+              _dr: BOOLEAN,
+              _stage: int,
+              _dme: BOOLEAN,
+              _hr: int,
+              _age_dme: int,
+              _rvo: BOOLEAN,
+              _crao: BOOLEAN,
+              _od: BOOLEAN,
+              _glaucoma: BOOLEAN,
+              _others: BOOLEAN,
+              _comment: str,
+              add_fail_callback: func,
+              add_succeed_callback: func):
+    pass
+
+
+def update_label_by_id(_id: int,
+                       _quality: BOOLEAN,
+                       _dr: BOOLEAN,
+                       _stage: int,
+                       _dme: BOOLEAN,
+                       _hr: int,
+                       _age_dme: int,
+                       _rvo: BOOLEAN,
+                       _crao: BOOLEAN,
+                       _od: BOOLEAN,
+                       _glaucoma: BOOLEAN,
+                       _others: BOOLEAN,
+                       _comment: str,
+                       find_fail_callback: func,
+                       update_fail_callback: func):
+    pass
+
+
+def delete_label_by_id(_id: int,
+                       find_fail_callback: func,
+                       delete_fail_callback: func,
+                       delete_succeed_callback: func):
+    pass
+
+
+def find_label_by_id(_id: int,
+                     find_fail_callback: func,
+                     find_succeed_callback: func):
+    pass
+
+if __name__ == '__main__':
+    init_db('root', '161518324', 'yelda', func)
+    create_table()
+
